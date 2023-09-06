a6cd4b047236f45d5acf0e0c4a64227a88a08e9c
jnxiong <janiexio@andrew.cmu.edu>
2023-09-06 13:44:40 -0400

    converted more functions, added disable eslint comments

--- src/groups/leave.ts
+++ src/groups/leave.ts
@@ -1,21 +1,55 @@
 import db from '../database';
 import user from '../user';
 import plugins from '../plugins';
-import cache from '../cache'
+import cache from '../cache';
 
 type GroupsType = {
-    leave: (groupNames: string[], uid: number) => Promise<void>
+    leave: (groupNames: string | string[], uid: number) => Promise<void>
     isMemberOfGroups: (uid: number, groupNames: string[]) => Promise<boolean[]>
     clearCache: (uid: number, groupsToLeave: string[]) => Promise<void>
-    getGroupsFields: (groupsToLeave: string[], str: string) => Promise<GroupsType>   //check str and return type
-    isPrivilegeGroups: (name: string) => Promise<boolean>
-    destroy: () => Promise<void>     //random stuff here
+    getGroupsFields: (groupsToLeave: string[], fields: string[]) => Promise<Group[]>
+    isPrivilegeGroup: (name: string) => boolean
     leaveAllGroups: (uid: number) => Promise<void>
+    destroy: (r:string) => Promise<void>
     rejectMembership: (groups: string[], uid: number) => Promise<void>
     kick: (uid: number, groupName: string, isOwner: boolean) => Promise<void>
 }
 
+type Group = {
+    name: string
+    memberCount: number
+    hidden: boolean
+}
+
+type UserDataType = {
+    groupTitleArray: string[]
+}
+
 module.exports = function (Groups: GroupsType) {
+    async function clearGroupTitleIfSet(groupNames: string[], uid: number) {
+        groupNames = groupNames.filter(groupName => groupName !== 'registered-users' && !Groups.isPrivilegeGroup(groupName));
+        if (!groupNames.length) {
+            return;
+        }
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        const userData: UserDataType = await user.getUserData(uid) as UserDataType;
+        if (!userData) {
+            return;
+        }
+
+        const newTitleArray: string[] = userData.groupTitleArray.filter(groupTitle => !groupNames.includes(groupTitle));
+        if (newTitleArray.length) {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            await db.setObjectField(`user:${uid}`, 'groupTitle', JSON.stringify(newTitleArray));
+        } else {
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            await db.deleteObjectField(`user:${uid}`, 'groupTitle');
+        }
+    }
+
     Groups.leave = async function (groupNames, uid) {
         if (Array.isArray(groupNames) && !groupNames.length) {
             return;
@@ -26,20 +60,24 @@ module.exports = function (Groups: GroupsType) {
 
         const isMembers: boolean[] = await Groups.isMemberOfGroups(uid, groupNames);
 
-        const groupsToLeave: string[] = groupNames.filter((groupName: string, index: number): boolean => isMembers[index]);
+        const groupsToLeave: string[] = groupNames.filter((groupName, index) => isMembers[index]);
         if (!groupsToLeave.length) {
             return;
         }
 
-        // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         await Promise.all([
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
             db.sortedSetRemove(groupsToLeave.map(groupName => `group:${groupName}:members`), uid),
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
             db.setRemove(groupsToLeave.map(groupName => `group:${groupName}:owners`), uid),
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
             db.decrObjectField(groupsToLeave.map(groupName => `group:${groupName}`), 'memberCount'),
         ]);
 
-        Groups.clearCache(uid, groupsToLeave);
+        await Groups.clearCache(uid, groupsToLeave);
         cache.del(groupsToLeave.map(name => `group:${name}:members`));
 
         const groupData = await Groups.getGroupsFields(groupsToLeave, ['name', 'hidden', 'memberCount']);
@@ -56,6 +94,8 @@ module.exports = function (Groups: GroupsType) {
         }
         if (visibleGroups.length) {
             promises.push(
+                // The next line calls a function in a module that has not been updated to TS yet
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
                 db.sortedSetAdd,
                 'groups:visible:memberCount',
                 visibleGroups.map(groupData => groupData.memberCount),
@@ -67,32 +107,18 @@ module.exports = function (Groups: GroupsType) {
 
         await clearGroupTitleIfSet(groupsToLeave, uid);
 
-        plugins.hooks.fire('action:group.leave', {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        await plugins.hooks.fire('action:group.leave', {
             groupNames: groupsToLeave,
             uid: uid,
         });
     };
 
-    async function clearGroupTitleIfSet(groupNames, uid) {
-        groupNames = groupNames.filter(groupName => groupName !== 'registered-users' && !Groups.isPrivilegeGroup(groupName));
-        if (!groupNames.length) {
-            return;
-        }
-        const userData = await user.getUserData(uid);
-        if (!userData) {
-            return;
-        }
-
-        const newTitleArray = userData.groupTitleArray.filter(groupTitle => !groupNames.includes(groupTitle));
-        if (newTitleArray.length) {
-            await db.setObjectField(`user:${uid}`, 'groupTitle', JSON.stringify(newTitleArray));
-        } else {
-            await db.deleteObjectField(`user:${uid}`, 'groupTitle');
-        }
-    }
-
     Groups.leaveAllGroups = async function (uid) {
-        const groups = await db.getSortedSetRange('groups:createtime', 0, -1);
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        const groups: string[] = await db.getSortedSetRange('groups:createtime', 0, -1) as string[];
         await Promise.all([
             Groups.leave(groups, uid),
             Groups.rejectMembership(groups, uid),
@@ -102,7 +128,9 @@ module.exports = function (Groups: GroupsType) {
     Groups.kick = async function (uid, groupName, isOwner) {
         if (isOwner) {
             // If the owners set only contains one member, error out!
-            const numOwners = await db.setCount(`group:${groupName}:owners`);
+            // The next line calls a function in a module that has not been updated to TS yet
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+            const numOwners: number = await db.setCount(`group:${groupName}:owners`) as number;
             if (numOwners <= 1) {
                 throw new Error('[[error:group-needs-owner]]');
             }
